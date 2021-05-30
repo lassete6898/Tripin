2c6558401175d45664c8ab4f78b85cb17341bbbd
Juan Lasso de la Vega <45948576+lassete6898@users.noreply.github.com>
2021-05-30 19:57:40 +0200

    login

--- js/login.js
+++ js/login.js
@@ -2,13 +2,13 @@ function login() {
   var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
 
-  //   window.alert(email, password);
-
-  if (email.length == 0) {
+  if (email.length == 0 && password.length == 0) {
+    window.alert("Introduce el email y la contraseña");
+  } else if (email.length == 0) {
     window.alert("Introduce el email");
   } else if (password.length == 0) {
     window.alert("Introduce la contraseña");
   } else {
-    location.href="user.html";
+    window.location.href = 'user.html';
   }
 }
