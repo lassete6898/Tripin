343919844eee4441f7bf821e887dd95b11ad7180
Juan Lasso de la Vega <45948576+lassete6898@users.noreply.github.com>
2021-06-06 21:04:09 +0200

    busqueda && user

--- js/user.js
+++ js/user.js
@@ -2,3 +2,7 @@ function closeSession() {
   alert("Sesión cerrada correctamente");
   window.location.href = "login.html";
 }
+
+function deleteBooking() {
+  window.alert("¡Reserva cancelada correctamente!")
+}
