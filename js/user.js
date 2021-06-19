d1e962b98eec1aab053abbe4385e211f75e44452
Merle66 <Merle66>
2021-06-19 19:10:52 +0200

    actualizacion js users

--- js/user.js
+++ js/user.js
@@ -6,3 +6,15 @@ function closeSession() {
 function deleteBooking() {
   window.alert("¡Reserva cancelada correctamente!")
 }
+
+function detalleReserva1(){
+  window.alert("Reserva para dos personas, del 25 de mayo al 20 de junio")
+}
+
+function detalleReserva2(){
+  window.alert("Reserva para dos personas, del 25 de junio al 15 de julio")
+}
+
+function detalleReserva3(){
+  window.alert("Reserva para dos personas, del 25 de julio al 1 de agosto")
+}
